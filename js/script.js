e779056b6d1c10ef9c6461dc9bf1992241decea2
Maggie McDonald <113684788+maggie-mcdonald@users.noreply.github.com>
2023-01-11 12:42:27 -0500

    Coded selector option

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function() {
-    const usersRequest = await fetch ("https://randomuser.me/api?results=5");
+const getData = async function(numUsers) {
+    const usersRequest = await fetch (`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();
-
-
     const userResults = data.results; //array of objects
     displayUsers(userResults);
 };
 
-getData ();
+getData (1);
 
 
 const displayUsers = function (userResults) {
@@ -29,3 +28,9 @@ const displayUsers = function (userResults) {
         randomFolks.append(userDiv);
     }
 };
+
+selectUserNumber.addEventListener("change", function (e) {
+    const numUsers = e.target.value;
+    getData (numUsers);
+
+});
